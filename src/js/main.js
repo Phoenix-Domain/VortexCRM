eb5dd1c7fb8f68051d73cd9685c1db34c4ab47ad
Batubo Victory <victorybatubo76@gmail.com>
2025-07-25 16:52:27 +0100

    Create user object

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 import '../css/style.css'
 
-
+let userId = 0;
+class MakeUser {
+  constructor(name,email,phone,service,source,date,time){
+    this.id = userId++;
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+    this.service = service;
+    this.source = source
+    this. date = date;
+    this.time = time;
+  }
+}
