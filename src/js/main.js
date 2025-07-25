e3097566a782c5c5b263f30c7219d281b10a1969
Batubo Victory <victorybatubo76@gmail.com>
2025-07-25 17:09:19 +0100

    Add pseudocode

--- src/js/main.js
+++ src/js/main.js
@@ -17,4 +17,15 @@ class MakeUser {
 const formInputs = document.querySelectorAll('input'); //All input elements in the DOM
 const userStatus = document.querySelector('#status');
 const submitBtn = document.querySelector('#submitBtn');
-const dashBoard = document.querySelector('#dashBoard')
+const dashBoard = document.querySelector('#dashBoard');
+
+//1. User types in value
+//2. I collect the value
+//3. I create a new user object
+//4. I pass the input values as parameters in the user object
+//5. I create 7 new td elements
+//6. I pass the appropriate values as it's textContent
+//7. I create a new tr element
+//8. I append all the td elements into the tr element
+//9. I append the tr element in the dashBoard as a new row
+//10. I clear the input
