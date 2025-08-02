12cd5bde79df544e25d46e214855998423a34192
Batubo Victory <victorybatubo76@gmail.com>
2025-08-02 10:15:45 +0100

    Create DOM module and export variables

---
+++ src/js/DOM.js
@@ -0,0 +1,8 @@
+export const formInputs = document.querySelectorAll('.formInput'); //All input elements in the DOM
+export const formUpdateInputs = document.querySelectorAll('.formUpdateInput');
+export const updateBtn = document.querySelector('#updateBtn');
+export const submitBtn = document.querySelector('#submitBtn');
+export const leadsList = document.querySelector('#leadsList');
+export const updateForm = document.querySelector('#updateForm');
+export const leadForm = document.querySelector('#leadForm');
+export const exportBtn = document.querySelector('#exportBtn');
